5837f98b6a6d5b8b85c9a7199214f9a0c93a3412
Alan Chang <alan@alanchang.xyz>
2021-03-23 01:25:12 -0700

    Add Spotify auth decline handling to login and add axios to frontend

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -13,13 +13,36 @@ const LogIn = (cookies) => {
             }
             return initial;
           }, {});
-    hash.expires_in = Number(hash.expires_in);
-
-    cookies.cookies.set('spotifyToken', hash.access_token, {
-        path: '/',
-        maxAge: hash.expires_in
-    });
-    // history.replace("/dashboard");
+    const query = window.location.search.substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            if (item) {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            }
+            return initial;
+        }, {});
+    
+    if ('error' in query) {
+        // send back to home page if errored out or abnormal
+        history.replace("/");
+    } else if (Object.keys(hash).length > 0) {
+        // to do: check for valid token, pass state in and check in return
+        if ('expires_in' in hash) {
+            try {
+                cookies.cookies.set('spotifyToken', hash.access_token, {
+                    path: '/',
+                    maxAge: Number(hash.expires_in)
+                });
+                history.replace("/dashboard");
+            } catch (err) {
+                history.replace("/");
+            }
+            // to do: add rows to tables in db for checkin + corresponding spotify status
+        } else {
+            history.replace("/");
+        } 
+    } 
     return null;
 }
 
