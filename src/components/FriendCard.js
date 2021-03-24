2cd969e36879e8d8e81ef51ed823983775cf7562
ayushib4 <ayushibatwara@gmail.com>
2021-03-23 18:00:55 -0700

    fixing the friend cards and more

--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -1,14 +1,18 @@
 import "./FriendCard.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 function FriendCard(props) {
-    return (
-         <Card className = "friend-card">
-            <Card.Title>{props.name}</Card.Title>
-        </Card>
-        
-    );
+  return (
+    <Card className="friend-card">
+      <Card.Body>
+        <Card.Title className="friend-name">{props.name}</Card.Title>
+        <Card.Img src={props.albumcover} />
+        <Card.Subtitle>{props.song}</Card.Subtitle>
+        <Card.Text>{props.artist}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default FriendCard;
