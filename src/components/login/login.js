3d274df5212d65018af0b3c08f09bc59e6d400da
Alan Chang <alan@alanchang.xyz>
2021-03-24 23:47:00 -0700

    Login checkin 1/2: Added call to backend to check in user, authenticate valid token via Spotify

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,9 +1,12 @@
 import { withCookies, Cookies } from 'react-cookie';
 import { instanceOf } from 'prop-types';
 import { useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+const axios = require('axios');
 
 const LogIn = (cookies) => {
     const history = useHistory();
+    const [res, setRes] = useState(null);
     const hash = window.location.hash.substring(1)
         .split('&')
         .reduce((initial, item) => {
@@ -23,26 +26,46 @@ const LogIn = (cookies) => {
             return initial;
         }, {});
     
-    if ('error' in query) {
-        // send back to home page if errored out or abnormal
-        history.replace("/");
-    } else if (Object.keys(hash).length > 0) {
-        // to do: check for valid token, pass state in and check in return
-        if ('expires_in' in hash) {
-            try {
-                cookies.cookies.set('spotifyToken', hash.access_token, {
-                    path: '/',
-                    maxAge: Number(hash.expires_in)
-                });
-                history.replace("/dashboard");
-            } catch (err) {
-                history.replace("/");
-            }
-            // to do: add rows to tables in db for checkin + corresponding spotify status
-        } else {
+    useEffect(() => {
+        const instance = axios.create({
+            baseURL: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/`,
+            withCredentials: true,
+        });
+        instance.post("/auth/checkin")
+          .then((response) => {
+            setRes(response.data);
+          })
+          .catch((err) => {
+            // console.log('Something went wrong fetching from the backend');
+            console.log(err.message);
+          });
+    }, []);
+    
+    useEffect(() => {
+        if (res === null) {return;}
+        if ('error' in query) {
             history.replace("/");
-        } 
-    } 
+        }
+        else if (Object.keys(hash).length > 0) {
+            // to do: check for valid token, pass state in and check in return
+            if ('expires_in' in hash) {
+                console.log(res);
+                if (res) {
+                    cookies.cookies.set('spotifyToken', hash.access_token, {
+                        path: '/',
+                        maxAge: Number(hash.expires_in)
+                    });
+                    history.replace("/dashboard");
+                } else {
+                    history.replace("/");
+                }
+                
+            } else {
+                history.replace("/");
+            } 
+        }
+    }, [res])
+
     return null;
 }
 
